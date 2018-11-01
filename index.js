ec2b0624a7280ac172bec195a3ba3a1470f858b3
semih Fatih <s_fatihh@hotmail.com>
2018-11-01 00:54:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function placeOrder(cardNumber) {
   //let total = this.total();
   if(cardNumber !== undefined ){
     cart.splice(0,cart.length);
-    return `Your total cost is $${this.total}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${this.total()}, which will be charged to the card ${cardNumber}.`
   }else{
     return "Sorry, we don't have a credit card on file for you."
   }
