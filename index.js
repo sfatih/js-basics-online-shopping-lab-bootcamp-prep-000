f1a2de122e3afea6bc1e49f6851397be526acb4f
semih Fatih <s_fatihh@hotmail.com>
2018-11-01 00:39:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ function viewCart() {
 
 function total() {
   // write your code here
+  
+  for(let i=0;i < cart.length; i++){
+    let total = 0;
+    total += cart[i].itemPrice;
+    return total;
+  }
 }
 
 function removeFromCart(item) {
