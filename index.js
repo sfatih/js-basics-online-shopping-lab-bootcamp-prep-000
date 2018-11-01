6cd09cc9ece717dcbc61bf822f52f7c9c0ab3788
semih Fatih <s_fatihh@hotmail.com>
2018-11-01 01:01:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function placeOrder(cardNumber) {
   let temp = total();
   if(cardNumber !== undefined ){
     cart.splice(0,cart.length);
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${temp}, which will be charged to the card ${cardNumber}.`
   }else{
     return "Sorry, we don't have a credit card on file for you."
   }
